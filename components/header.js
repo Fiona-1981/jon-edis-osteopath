a3c15645924f2522679b4ab608503e26cf13812c
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-08-08 21:00:32 +0100

    Flexbox for top header

--- components/header.js
+++ components/header.js
@@ -36,14 +36,26 @@ class Header extends HTMLElement {
           background: #125688;
           color: white;
         }
+
+        .top-header {
+          display: flex;
+          justify-content: flex-end;
+          justify-content: space-around;
+          padding: 10px;
+        }
+
+        .main-header {
+          text-align: center;
+        }
       </style>
       <header>
-        Phone: 07871 836785
-        Email: <a href="mailto:jonedisosteopath@gmail.com">jonedisosteopath@gmail.com</a>
-        <a href="https://www.facebook.com/jonathanedisosteopath" class="fa fa-facebook" target="_blank"></a>
-        <a href="https://www.instagram.com/jonedis_osteo/" class="fa fa-instagram" target="_blank"></a>
+        <div class="top-header"><div>Phone: 07871 836785
+        Email: <a href="mailto:jonedisosteopath@gmail.com">jonedisosteopath@gmail.com</a></div>
+        <div><a href="https://www.facebook.com/jonathanedisosteopath" class="fa fa-facebook" target="_blank"></a>
+        <a href="https://www.instagram.com/jonedis_osteo/" class="fa fa-instagram" target="_blank"></a></div></div>
+        <div class="main-header">
         <a href="index.html"><h1><img src="../logos/je-logo.gif" alt="Jonathan Edis Logo">Jonathan Edis Osteopath</h1>
-        <h2>Osteopathy & Osteopathic Lecturing</h2></a>
+        <h2>Osteopathy & Osteopathic Lecturing</h2></a></div>
       </header>
     `;
   }
